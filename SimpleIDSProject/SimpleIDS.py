c03694dabba3a3e67a32639f834faf82a6f1953b
HiraethAwaits <146136772+HiraethAwaits@users.noreply.github.com>
2025-07-28 13:18:52 -0700

    Finished the skeleton of the project, will need to make a Auth Log that works

--- SimpleIDSProject/SimpleIDS.py
+++ SimpleIDSProject/SimpleIDS.py
@@ -21,4 +21,20 @@ def simulate_log_read():
 def check_line(line, currect_line):
     match = FAIL_REGEX.search(line)
     if match:
-        
+        ip = match.group(1)
+        failed_attempts[ip].append(current_time)
+
+        failed_attempts[ip] = [t for t in failed_attempts[ip] if current_time - t < TIME_WINDOW]
+
+        if len(failed_attempts[ip]) >= THRESHOLD:
+            alert(ip)
+            failed_attempts[ip].clear()
+
+def main():
+    print(f"[*] Simulated IDS monitoring {LOG_FILE} ... \n")
+    for line in simulate_log_read():
+        now = time.time()
+        check_line(line, now)
+
+if __name__ == "__main__":
+    main()
