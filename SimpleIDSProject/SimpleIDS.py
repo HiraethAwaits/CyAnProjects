594614d0be558fb37312b2193668a8cfe25d98a6
HiraethAwaits <146136772+HiraethAwaits@users.noreply.github.com>
2025-07-28 13:07:04 -0700

    Made a Auth File, alongside the beginnings of my Simple IDS

---
+++ SimpleIDSProject/SimpleIDS.py
@@ -0,0 +1,24 @@
+import time
+import re
+from collections import defaultdict
+
+LOG_FILE = "auth.log"
+FAIL_REGEX = re.compile(r"failed password for .* from (\d+\.\d+\.\d+\.\d+)")
+THRESHOLD = 5
+TIME_WINDOW = 60
+
+failed_attempts = defaultdict(list)
+
+def alert(ip):
+    print(f"[!] ALERT: Multiple failed login attempts from {ip}!")
+
+def simulate_log_read():
+    with open(LOG_FILE, "r") as f:
+        for line in f:
+            yield line
+            time.sleep(0.5)
+
+def check_line(line, currect_line):
+    match = FAIL_REGEX.search(line)
+    if match:
+        
